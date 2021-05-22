93f8f618d63daa18809ae3b7d92b582b1b2b819d
Yueyue Wang <yueywang@microsoft.com>
2021-05-22 19:53:00 +0800

    test aml-run pipeline

---
+++ code/train/run_config.py
@@ -0,0 +1,18 @@
+from azureml.pipeline.core import PipelineEndpoint
+from azureml.core import Workspace
+
+def main(workspace):
+    ws = Workspace.from_config()
+    print(ws)
+    print("------------")
+
+    published_pipeline = PipelineEndpoint.get(workspace=ws, name="aml-run-val")
+    print(published_pipeline)
+
+    print("------------")
+
+    pipeline_run = published_pipeline.submit("aml-run-val")
+
+    print("run completed")
+    
+    return pipeline_run
