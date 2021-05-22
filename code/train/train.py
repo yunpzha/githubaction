d7e46e08a78b94b21b0c768db97a6116df139002
王脸小 <52517818+wang-piaoliang@users.noreply.github.com>
2021-05-23 00:04:02 +0800

    Update train.py

--- code/train/train.py
+++ code/train/train.py
@@ -1,6 +1,25 @@
 from azureml.pipeline.core import PipelineEndpoint
 from azureml.core import Workspace
-ws = Workspace.from_config()
+# ws = Workspace.from_config()
+
+import os
+from azureml.core.authentication import ServicePrincipalAuthentication
+
+svc_pr_password = "QOFpAEY8.0o7~_Z61cX7YkPFnw~_-M_QcI"
+
+svc_pr = ServicePrincipalAuthentication(
+    tenant_id="b5d973af-dab6-44d1-9141-cb325843ffd2",
+    service_principal_id="b5d973af-dab6-44d1-9141-cb325843ffd2",
+    service_principal_password=svc_pr_password)
+
+
+ws = Workspace(
+    subscription_id="2acfc131-8c35-4698-95d8-df43d793ca8b",
+    resource_group="MLOpsGithub",
+    workspace_name="demoAzMLWorkspace",
+    auth=svc_pr
+    )
+
 
 print("!!!Start")
 print(ws)
@@ -14,3 +33,4 @@ print("------------")
 pipeline_run = published_pipeline.submit("aml-run-val")
 
 print("run completed")
+
