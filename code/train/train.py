0bf57788f5672735fb830f2cfc20a04085eac722
SriniGajjela <32370474+SriniGajjela@users.noreply.github.com>
2021-05-22 13:40:06 -0500

    Update train.py

--- code/train/train.py
+++ code/train/train.py
@@ -8,14 +8,20 @@ from azureml.core.authentication import ServicePrincipalAuthentication
 
 def main(workspace):
     print(workspace)
+    published_pipeline = PipelineEndpoint.get(workspace=workspace, name="aml-run-val")
+    print(published_pipeline)
 
+    print("------------")
+    pipeline_run = published_pipeline.submit("aml-run-val")
+
+    print("run completed")
+
+"""
 svc_pr_password = "QOFpAEY8.0o7~_Z61cX7YkPFnw~_-M_QcI"
-
 svc_pr = ServicePrincipalAuthentication(
     tenant_id="72f988bf-86f1-41af-91ab-2d7cd011db47",
     service_principal_id="b5d973af-dab6-44d1-9141-cb325843ffd2",
     service_principal_password=svc_pr_password)
-
 
 ws = Workspace(
     subscription_id="2acfc131-8c35-4698-95d8-df43d793ca8b",
@@ -23,18 +29,8 @@ ws = Workspace(
     workspace_name="demoAzMLWorkspace",
     auth=svc_pr
     )
+"""
 
 
-print("!!!Start")
-print(ws)
-print("------------")
 
-published_pipeline = PipelineEndpoint.get(workspace=workspace, name="aml-run-val")
-print(published_pipeline)
-
-print("------------")
-
-pipeline_run = published_pipeline.submit("aml-run-val")
-
-print("run completed")
 
